ac954626c27fe1893f499604b8b0e6857b8fa2b7
brex-corndel <jeremy.brex@aexp.com>
2021-02-25 20:52:09 +0000

    Added Selenium end to end tests

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -11,3 +11,19 @@ test_app = app.create_app()
 
 with test_app.test_client() as client: 
     yield client
+
+@patch('requests.get')
+def test_index_page(mock_get_requests, client):
+# Replace call to requests.get(url) with our own function
+    mock_get_requests.side_effect = mock_get_lists 
+    response = client.get('/') 
+
+def mock_get_lists(url, params):
+    if url == f'https://api.trello.com/1/boards/
+        {TEST_BOARD_ID}/lists': response = Mock()
+
+# sample_trello_lists_response should point to some test response data
+
+    response.json.return_value = sample_trello_lists_response
+    return response 
+return None
