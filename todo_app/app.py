f3a4ae4388b8cd3c0a1f1e8b34442a88e5edd076
brex-corndel <jeremy.brex@aexp.com>
2021-02-25 20:32:38 +0000

    Added Integration Tests

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,28 +4,34 @@ import todo_app.trello_client as trello_client
 from todo_app.view_model import ViewModel
 from todo_app.data.todo_item import TodoItem
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    raw_cards = trello_client.get_cards_for_board()
-    todo_items = [TodoItem.from_raw_trello_card(card) for card in raw_cards]
+def create_app():
+   app = Flask(__name__) 
+   app.config.from_object('app_config.Config')
 
-    item_view_model = ViewModel(todo_items)
+    @app.route('/')
+    def index():
+        raw_cards = trello_client.get_cards_for_board()
+        todo_items = [TodoItem.from_raw_trello_card(card) for card in raw_cards]
+        item_view_model = ViewModel(todo_items)
+        return render_template('index.html', view_model=item_view_model)
 
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/add_item', methods=['POST'])
+    def add_item():
+        item_title = request.form['Title']
+        trello_client.add_todo(item_title)
+        return redirect('/')
 
-@app.route('/add_item', methods=['POST'])
-def add_item():
-    item_title = request.form['Title']
-    trello_client.add_todo(item_title)
-    return redirect('/')
+    @app.route('/complete_item', methods=['POST'])
+    def complete_item():
+        todo_id = request.form['id']
+        trello_client.complete_todo(todo_id)
+        return redirect('/')
 
-@app.route('/complete_item', methods=['POST'])
-def complete_item():
-    todo_id = request.form['id']
-    trello_client.complete_todo(todo_id)
-    return redirect('/')
+return app
 
 if __name__ == '__main__':
     app.run()
+
+
